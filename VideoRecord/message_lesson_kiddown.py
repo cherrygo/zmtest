7b5ec3b69711f6563a99eaaf21a3be0670f6ae54
cherrygo <88423909+cherrygo@users.noreply.github.com>
2021-08-04 16:57:56 +0800

    Delete VideoRecord directory

--- VideoRecord/message_lesson_kiddown.py
+++
@@ -1,9 +0,0 @@
-import message_record_mq
-
-def sendmq(lessonuid):
-    topic = "*"
-    body = {"type":"down","userId":1426704877,"time":1611716603875,"lessonUID":lessonuid}
-    message_record_mq.send(topic, '', body)
-
-if __name__ == '__main__' :
-    sendmq('f6f3b5fb5a6b420c8fffd039b4d64ea3')
